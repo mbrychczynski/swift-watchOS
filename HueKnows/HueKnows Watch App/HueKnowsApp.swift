21ec0b7f7a5252645a3dc81be1cf336943b62444
Mateusz Brychczynski <mat.brych@gmail.com>
2023-06-17 10:51:58 +0100

    Add files via upload

---
+++ HueKnows/HueKnows Watch App/HueKnowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HueKnowsApp.swift
+//  HueKnows Watch App
+//
+//  Created by Mateusz Brychczynski on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct HueKnows_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
