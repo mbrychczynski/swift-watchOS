8f9e6009fc058259bc8972e8a192b308a59df835
Mateusz Brychczynski <mat.brych@gmail.com>
2023-06-26 14:06:46 +0100

    Add files via upload

---
+++ SafeCrack/SafeCrack Watch App/SafeCrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeCrackApp.swift
+//  SafeCrack Watch App
+//
+//  Created by Mateusz Brychczynski on 26/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SafeCrack_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
